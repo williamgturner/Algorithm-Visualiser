120d26426f547ce2c93a7b3223a71af07330418c
Will Turner <willguyturner@gmail.com>
2025-01-22 20:17:45 +1300

    Added documentation.
    Added docstrings & comments for classes and methods.

--- main.py
+++ main.py
@@ -5,37 +5,42 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class navbarFrame(customtkinter.CTkFrame):
+    """Frame that holds buttons to select algorithm"""
     def __init__(self, master):
         super().__init__(master)
 
 class canvasFrame(customtkinter.CTkFrame):
+    """Frame that holds the matplotlib figure"""
     def __init__(self, master):
         super().__init__(master)
     
     def embed_plot(self, plot):
-            fig = plot.get_figure()
+        """Embeds plot into frame
+        
+        Creates a regular tk canvas which then holds the matplotlib
+        figure. The figure is adjusted to fit within the viewport."""
+        fig = plot.get_figure()
 
-            fig.patch.set_facecolor('none')
-            frame_width = self.winfo_width()
-            frame_height = self.winfo_height()
-            
-            aspect_ratio = frame_width / frame_height
-            fig_width = frame_width * 0.6
-            fig_height = fig_width / aspect_ratio
-            
-            # Adjust the figure size
-            fig.set_size_inches(fig_width / 100, fig_height / 100)  # Convert pixels to inches (100 dpi)
-            
-            # Optionally, use tight_layout to make the plot fit better
-            fig.tight_layout()
+        fig.patch.set_facecolor('none')
+        frame_width = self.winfo_width()
+        frame_height = self.winfo_height()
+        
+        aspect_ratio = frame_width / frame_height
+        fig_width = frame_width * 0.6
+        fig_height = fig_width / aspect_ratio
+        
+        fig.set_size_inches(fig_width / 100, fig_height / 100)  # Convert pixels to inches (100 dpi)
+        
+        fig.tight_layout()
 
-            canvas =FigureCanvasTkAgg(fig, master = self)
-            canvas.draw()
-            canvas_widget = canvas.get_tk_widget()
-            canvas.get_tk_widget().pack(padx=5, pady=(50,0), fill=tk.BOTH, expand=True)
-            canvas_widget.config(bg='lightgray')
+        canvas =FigureCanvasTkAgg(fig, master = self)
+        canvas.draw()
+        canvas_widget = canvas.get_tk_widget()
+        canvas.get_tk_widget().pack(padx=5, pady=(50,0), fill=tk.BOTH, expand=True)
+        canvas_widget.config(bg='lightgray')
 
 class descriptionFrame(customtkinter.CTkFrame):
+    """Frame that holds the description text for given algorithm"""
     def __init__(self, master):
         super().__init__(master)
 
@@ -71,18 +76,17 @@ class App(customtkinter.CTk):
         self.desc_text.insert(0.0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis pellentesque lectus. Duis maximus augue a nisi egestas, id vehicula sapien fringilla. Sed porta dui vulputate turpis malesuada, quis venenatis neque maximus. Phasellus efficitur tellus in risus ultrices lobortis. Mauris sed justo hendrerit eros convallis rutrum. Aliquam fringilla tellus a velit efficitur, a volutpat lectus imperdiet. In non tempus nulla. Praesent in massa vel erat sollicitudin fermentum. Nam consectetur nunc leo, et viverra quam maximus quis. Praesent id elit urna. Morbi non lorem blandit, placerat leo et, cursus erat. Suspendisse vestibulum, dolor a cursus sagittis, ipsum mi feugiat justo, sit amet condimentum augue dui vitae urna. Suspendisse condimentum, lectus quis porttitor ornare, lectus eros faucibus leo, vitae semper dui ligula nec leo. Nulla consectetur tellus ac maximus interdum. Nulla facilisi.")
         self.desc_text.grid(row=1, column=0, padx=5, pady=5, sticky="nesw")
 
-        self.binSearchButton = customtkinter.CTkButton(self.nav_frame, text = "BinarySearch", command=self.button_callback)
+        self.binSearchButton = customtkinter.CTkButton(self.nav_frame, text = "Binary Search", command=self.button_callback)
         self.binSearchButton.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
+        # init seaborn plot
         self.plot_data = sns.load_dataset("penguins")
         self.plot = sns.barplot(data=self.plot_data, x="species", y="bill_length_mm")
         plt.axis('off')
-        #sns.despine()
-        #self.plot.get_figure().set_size_inches(2,1)
         self.canvas_frame.embed_plot(self.plot)
         
         
-        self.focus_force()
+        self.focus_force() # bring window into focus on app open
 
     def start_vis(self):
         print("Visualisation Started")
